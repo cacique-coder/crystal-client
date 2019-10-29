b3c986a9d78e1b1d3d927a14f83825047f271df2
Kim Burgess <kim@acaprojects.com>
2019-10-29 16:05:21 +1000

    refactor(api_wrapper) macros for params and body

--- src/acaengine/api_wrapper.cr
+++ src/acaengine/api_wrapper.cr
@@ -29,29 +29,94 @@ class ACAEngine::APIWrapper
     token_provider.authenticate connection
   end
 
+  # Build a `HTTP::Params` instance from local context.
+  #
+  # Accepts a set of potentially nilable references and expands into params
+  # instance containing any of these entities with a non-nil value.
+  macro params_from(*keys)
+    HTTP::Params.build do |param|
+      {% for key in keys %}
+        param.add {{key.id.stringify}}, {{key.id}}.to_s unless {{key.id}}.nil?
+      {% end %}
+    end
+  end
+
+  # Builds a `HTTP::Params` instance from local method arguments.
+  #
+  # Maps any method arguments that contain a value other than their default
+  # into params on the expanded object.
+  #
+  # The optional *except* parameter may be used to filter to a subset of
+  # arguments only.
+  macro params_from_args(except = [id])
+    {% except = [except] unless except.is_a? ArrayLiteral %}
+    {% except = except.map &.id %}
+    {% args = @def.args.reject { |arg| except.includes? arg.name } %}
+
+    HTTP::Params.build do |param|
+      {% for arg in args %}
+        # Always include required args, the compiler doesn't like Nop's
+        {% if arg.default_value.is_a? Nop %}
+          param.add {{arg.name.stringify}}, {{arg.name}}.to_s
+        {% else %}
+          param.add {{arg.name.stringify}}, {{arg.name}}.to_s \
+            unless {{arg.name}} == {{arg.default_value}}
+        {% end %}
+      {% end %}
+    end
+  end
+
+  # Builds a JSON string from local context.
+  #
+  # Accepts a set of potentially nilable references and builds a `String`
+  # containing JSON representation of all with non-nil values.
+  macro json_from(*keys)
+    JSON.build do |json|
+      json.object do
+        {% for key in keys %}
+          json.field {{key.id.stringify}}, {{key.id}} unless {{key.id}}.nil?
+        {% end %}
+      end
+    end
+  end
+
+  # Builds a JSON string from local method arguments.
+  #
+  # Maps any method arguments that contain a value other than their default
+  # into params on the expanded object.
+  #
+  # The optional *except* parameter may be used to filter to a subset of
+  # arguments only.
+  macro json_from_args(except = [id])
+    {% except = [except] unless except.is_a? ArrayLiteral %}
+    {% except = except.map &.id %}
+    {% args = @def.args.reject { |arg| except.includes? arg.name } %}
+    JSON.build do |json|
+      json.object do
+        {% for arg in args %}
+          # Always include required args
+          {% if arg.default_value.is_a? Nop %}
+            json.field {{arg.name.stringify}}, {{arg.name}}
+          {% else %}
+            json.field {{arg.name.stringify}}, {{arg.name}} \
+              unless {{arg.name}} == {{arg.default_value}}
+          {% end %}
+        {% end %}
+      end
+    end
+  end
+
   {% for method in %w(get post put head delete patch options) %}
     # Executes a {{method.id.upcase}} request on the client connection.
     #
-    # The response status will be automatically checked and a ACAEngine::Client::Error raised if
-    # unsuccessful.
+    # The response status will be automatically checked and a
+    # `ACAEngine::Client::Error` raised if unsuccessful.
     protected def {{method.id}}(path, headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType? = nil)
+      headers ||= HTTP::Headers.new
+      headers["Content-Type"] = "application/json" unless body.nil?
       response = connection.{{method.id}} path, headers, body
       raise API::Error.from_response(response) unless response.success?
       response
-    end
-
-    # Executes a {{method.id.upcase}} request on the client connection with a JSON body formed from
-    # the passed serializable object.
-    protected def {{method.id}}(path, body)
-      headers = HTTP::Headers.new
-      headers["Content-Type"] = "application/json"
-      {{method.id}} path, headers, body.to_json
-    end
-
-    # :ditto:
-    protected def {{method.id}}(path, headers : HTTP::Headers, body)
-      headers["Content-Type"] = "application/json"
-      {{method.id}} path, headers, body.to_json
     end
   {% end %}
 end
--- src/acaengine/api_wrapper/systems.cr
+++ src/acaengine/api_wrapper/systems.cr
@@ -5,7 +5,7 @@ require "../api/models/system"
 class ACAEngine::APIWrapper
   # Retrieves systems.
   #
-  # Results maybe filtered by specifying a *query* to search across system
+  # Results maybe filtered by specifying a query - *q* - to search across system
   # attributes. A small query language is supported within this:
   #
   # Operator | Action
@@ -19,13 +19,8 @@ class ACAEngine::APIWrapper
   # `~N`     | Specifies slop amount (deviation) after a phrase
   #
   # Up to *limit* systems will be returned, with a paging based on *offset*.
-  def systems(query : String? = nil, limit : Int? = nil, offset : Int? = nil)
-    params = HTTP::Params.build do |param|
-      param.add "q", query.to_s unless query.nil?
-      param.add "limit", limit.to_s unless limit.nil?
-      param.add "offset", offset.to_s unless offset.nil?
-    end
-    response = get "/api/control/systems?#{params}"
+  def systems(q : String? = nil, limit : Int? = nil, offset : Int? = nil)
+    response = get "/api/control/systems?#{params_from_args}"
     QueryResult(System).from_json response.body
   end
 end
