295d687533465fd71af57c9c7dbe8378e3fdd8e5
Stephen von Takach <steve@vontaka.ch>
2020-10-20 10:51:23 +1100

    feat(zone model): add additional database fields

--- src/placeos/api/models/zone.cr
+++ src/placeos/api/models/zone.cr
@@ -11,8 +11,29 @@ module PlaceOS::Client::API::Models
     # A human readable identifier.
     getter name : String
 
+    # A human readable identifier for displaying on interfaces
+    getter display_name : String?
+
+    # Geo-location string (lat,long) or any other location
+    getter location : String?
+
     # Markdown formatted text that describes the zone.
     getter description : String?
+
+    # Could be used as floor code or building code etc
+    getter code : String?
+
+    # Could be used as floor type or building type etc
+    getter type : String?
+
+    # Could be used as desk count for a level
+    getter count : Int32
+
+    # Could be used as people capacity
+    getter capacity : Int32
+
+    # Map identifier, could be a URL or id
+    getter map_id : String?
 
     # Space seperated list of tags for categorizing the zone.
     getter tags : Array(String) = [] of String
