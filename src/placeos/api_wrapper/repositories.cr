533b7e94db565e85bcda0f357037a085accf7686
Gab Fitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-10-26 12:51:08 +1000

    reverted chanages to repository files

--- src/placeos/api_wrapper/repositories.cr
+++ src/placeos/api_wrapper/repositories.cr
@@ -2,7 +2,7 @@ require "./endpoint"
 
 module PlaceOS
   class Client::APIWrapper::Repositories < Client::APIWrapper::Endpoint
-    # include Client::APIWrapper::Endpoint::Fetch(Repository)
+    include Client::APIWrapper::Endpoint::Fetch(Repository)
     include Client::APIWrapper::Endpoint::Destroy
 
     getter base : String = "#{API_ROOT}/repositories"
