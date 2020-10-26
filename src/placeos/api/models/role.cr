331a2d334d230c2e7109c730dec951fc9706743c
Gab Fitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-10-27 09:43:00 +1000

    style(role enum): include Client::API::Models allows for Role:{role}

--- src/placeos/api/models/role.cr
+++ src/placeos/api/models/role.cr
@@ -1,9 +1,4 @@
-#  require "models/driver"
-
 module PlaceOS::Client::API::Models
-  # NOTE: active-model is having an issue with resolving a type.
-  #       The obvious ideal is the alias below with `require "models/driver"`
-  # alias Role = PlaceOS::Models::Driver::Role
   enum Role
     SSH       =  0
     Device    =  1
