a3ca125e6889d501decc05113e428edb6d95f81c
Gab Fitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-10-28 17:08:22 +1000

    test: settings common methods

--- spec/placeos/api_wrapper/settings_spec.cr
+++ spec/placeos/api_wrapper/settings_spec.cr
@@ -5,62 +5,83 @@ module PlaceOS
     api = PlaceOS::Client::APIWrapper.new DOMAIN
     client = Client::APIWrapper::Settings.new api
 
-    # settings_json = <<-JSON
-    # [
-    #   {
-    #     "id": "setting-oOj2lGgsz",
-    #     "parent_id": ""
-    #     "name": "Place",
-    #     "description": null,
-    #     "tags": ["org"],
-    #     "triggers": [],
-    #     "created_at": 1555995992,
-    #     "updated_at": 1555996000,
-    #     "count": 0,
-    #     "capacity": 2
-    #   }
-    # ]
-    # JSON
+    settings_json = <<-JSON
+    [
+      {
+        "id": "settings-oOj2lGgsz",
+        "parent_id": "Place",
+        "encryption_level": "None",
+        "parent_type": "Driver"
+      }
+    ]
+    JSON
 
-    # settings = Array(JSON::Any).from_json(settings_json).map &.to_json
+    settings = Array(JSON::Any).from_json(settings_json).map &.to_json
 
     describe "#search" do
-      # it "enumerates all settings" do
-      #   WebMock
-      #     .stub(:get, DOMAIN + client.base)
-      #     .with(query: {"limit" => "20", "offset" => "0"}, headers: HEADERS)
-      #     .to_return(body: settings_json)
-      #   result = client.search
-      #   result.size.should eq(1)
-      #   result.first.should be_a(Client::API::Models::Setting)
-      #   result.first.name.should eq("Place")
-      # end
+      it "enumerates all settings" do
+        WebMock
+          .stub(:get, DOMAIN + client.base)
+          .with(query: {"limit" => "20", "offset" => "0"}, headers: HEADERS)
+          .to_return(body: settings_json)
+        result = client.search
+        result.size.should eq(1)
+        result.first.should be_a(PlaceOS::Client::API::Models::Settings)
+        result.first.parent_id.should eq("Place")
+      end
 
-      # it "provides setting search" do
-      #   WebMock
-      #     .stub(:get, DOMAIN + client.base)
-      #     .with(query: {"q" => "Place", "limit" => "20", "offset" => "0"}, headers: HEADERS)
-      #     .to_return(body: drivers_json)
-      #   result = client.search q: "Place"
-      #   result.size.should eq(2)
-      #   result.first.name.should eq("Place")
-      # end
+      it "provides settings search" do
+        WebMock
+          .stub(:get, DOMAIN + client.base)
+          .with(query: {"q" => "Place", "limit" => "20", "offset" => "0"}, headers: HEADERS)
+          .to_return(body: settings_json)
+        result = client.search q: "Place"
+        result.size.should eq(1)
+        result.first.parent_id.should eq("Place")
+      end
     end
 
-    describe "#fetch" do
+    it "#fetch" do
+      WebMock
+        .stub(:get, DOMAIN + "#{client.base}/settings-oOj2lGgsz")
+        .to_return(body: settings.first)
+      result = client.fetch "settings-oOj2lGgsz"
+      result.should be_a(Client::API::Models::Settings)
+      result.to_json.should eq("{\"id\":\"settings-oOj2lGgsz\",\"encryption_level\":0,\"settings_string\":\"{}\",\"keys\":[],\"parent_id\":\"Place\",\"parent_type\":1}")
     end
 
     it "#destroy" do
       WebMock
-        .stub(:delete, DOMAIN + "#{client.base}/setting-oOj2lGgsz")
-      result = client.destroy "setting-oOj2lGgsz"
+        .stub(:delete, DOMAIN + "#{client.base}/settings-oOj2lGgsz")
+      result = client.destroy "settings-oOj2lGgsz"
       result.should be_nil
     end
 
-    describe "#create" do
+    it "#create" do
+      body = {parent_id: "Foo", encryption_level: "None", parent_type: "Driver", settings_string: "settings", settings_id: "unique id", keys: ["key1", "key2"]}.to_json
+      WebMock
+        .stub(:post, DOMAIN + client.base)
+        .with(
+          headers: HTTP::Headers{"Content-Type" => "application/json"},
+          body: body
+        )
+        .to_return(body: settings.first)
+      result = client.create(parent_id: "Foo", encryption_level: "None", parent_type: "Driver", settings_string: "settings", settings_id: "unique id", keys: ["key1", "key2"])
+      result.should be_a(Client::API::Models::Settings)
+      result.to_json.should eq("{\"id\":\"settings-oOj2lGgsz\",\"encryption_level\":0,\"settings_string\":\"{}\",\"keys\":[],\"parent_id\":\"Place\",\"parent_type\":1}")
     end
 
-    describe "#update" do
+    it "#update" do
+      WebMock
+        .stub(:put, DOMAIN + "#{client.base}/settings-oOj2lGgsz")
+        .with(
+          headers: {"Content-Type" => "application/json"},
+          body: {parent_id: "Foo"}.to_json
+        )
+        .to_return(body: settings.first)
+      result = client.update "settings-oOj2lGgsz", parent_id: "Foo", encryption_level: nil, parent_type: nil, settings_string: nil, settings_id: nil, keys: nil
+      result.should be_a(Client::API::Models::Settings)
+      result.to_json.should eq("{\"id\":\"settings-oOj2lGgsz\",\"encryption_level\":0,\"settings_string\":\"{}\",\"keys\":[],\"parent_id\":\"Place\",\"parent_type\":1}")
     end
 
     describe "#history" do
