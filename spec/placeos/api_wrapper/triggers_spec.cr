deeae8c1471b71dfa629303ab711124475fd13ba
Gab Fitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-10-30 15:43:40 +1000

    chore: triggers specs:

--- spec/placeos/api_wrapper/triggers_spec.cr
+++ spec/placeos/api_wrapper/triggers_spec.cr
@@ -2,19 +2,92 @@ require "../../spec_helper"
 
 module PlaceOS
   describe Client::APIWrapper::Triggers do
+    api = PlaceOS::Client::APIWrapper.new DOMAIN
+    client = Client::APIWrapper::Triggers.new api
+    
+    triggers_json = <<-JSON
+    [
+      {
+        "name": "Place",
+        "authority_id": "hello",
+        "description": null,
+        "tags": ["org"],
+        "triggers": [],
+        "created_at": 1555995992,
+        "updated_at": 1555996000,
+        "id": "trigger-oOj2lGgsz",
+        "count": 0,
+        "capacity": 2
+      }
+    ]
+    JSON
+
+    triggers = Array(JSON::Any).from_json(triggers_json).map &.to_json
+
     describe "#search" do
+      it "enumerates all triggers" do
+        WebMock
+          .stub(:get, DOMAIN + client.base)
+          .with(query: {"authority_id" => "hello", "limit" => "20", "offset" => "0"}, headers: HEADERS)
+          .to_return(body: triggers_json)
+        result = client.search "hello"
+        result.size.should eq(1)
+        result.first.should be_a(PlaceOS::Model::Trigger)
+        result.first.name.should eq("Place")
+      end
+
+      it "provides trigger search" do
+        WebMock
+          .stub(:get, DOMAIN + client.base)
+          .with(query: {"authority_id" => "hello", "limit" => "20", "offset" => "0"}, headers: HEADERS)
+          .to_return(body: triggers_json)
+        result = client.search "hello"
+        result.size.should eq(1)
+        result.first.name.should eq("Place")
+      end
     end
 
-    describe "#fetch" do
+    it "#fetch" do
+      WebMock
+        .stub(:get, DOMAIN + "#{client.base}/trigger-oOj2lGgsz")
+        .to_return(body: triggers.first)
+      result = client.fetch "trigger-oOj2lGgsz"
+      result.should be_a(PlaceOS::Model::Trigger)
+      result.to_json.should eq("{\"created_at\":1555995992,\"updated_at\":1555996000,\"name\":\"Place\",\"description\":\"\",\"actions\":{\"functions\":[],\"mailers\":[]},\"conditions\":{\"comparisons\":[],\"time_dependents\":[]},\"debounce_period\":0,\"important\":false,\"enable_webhook\":false,\"supported_methods\":[\"POST\"]}")
     end
 
-    describe "#destroy" do
+    it "#destroy" do
+      WebMock
+        .stub(:delete, DOMAIN + "#{client.base}/trigger-oOj2lGgsz")
+      result = client.destroy "trigger-oOj2lGgsz"
+      result.should be_nil
     end
 
-    describe "#create" do
+    it "#create" do
+      body = {control_system_id: "hello", name: "Place"}.to_json
+      WebMock
+        .stub(:post, DOMAIN + client.base)
+        .with(
+          headers: HTTP::Headers{"Content-Type" => "application/json"},
+          body: body
+        )
+        .to_return(body: triggers.first)
+      result = client.create(control_system_id: "hello", name: "Place")
+      result.should be_a(PlaceOS::Model::Trigger)
+      result.to_json.should eq("{\"created_at\":1555995992,\"updated_at\":1555996000,\"name\":\"Place\",\"description\":\"\",\"actions\":{\"functions\":[],\"mailers\":[]},\"conditions\":{\"comparisons\":[],\"time_dependents\":[]},\"debounce_period\":0,\"important\":false,\"enable_webhook\":false,\"supported_methods\":[\"POST\"]}")
     end
 
-    describe "#update" do
+    it "#update" do
+      WebMock
+        .stub(:put, DOMAIN + "#{client.base}/trigger-oOj2lGgsz")
+        .with(
+          headers: {"Content-Type" => "application/json"},
+          body: {control_system_id: "foo", name: "Foo"}.to_json
+        )
+        .to_return(body: triggers.first)
+      result = client.update "trigger-oOj2lGgsz", control_system_id: "foo", name: "Foo"
+      result.should be_a(PlaceOS::Model::Trigger)
+      result.to_json.should eq("{\"created_at\":1555995992,\"updated_at\":1555996000,\"name\":\"Place\",\"description\":\"\",\"actions\":{\"functions\":[],\"mailers\":[]},\"conditions\":{\"comparisons\":[],\"time_dependents\":[]},\"debounce_period\":0,\"important\":false,\"enable_webhook\":false,\"supported_methods\":[\"POST\"]}")
     end
 
     describe "#instances" do
--- src/placeos/api_wrapper/triggers.cr
+++ src/placeos/api_wrapper/triggers.cr
@@ -2,9 +2,12 @@ require "./endpoint"
 
 module PlaceOS
   class Client::APIWrapper::Triggers < Client::APIWrapper::Endpoint
-    include Client::APIWrapper::Endpoint::Fetch(Trigger)
-    include Client::APIWrapper::Endpoint::Destroy
+    include JSON::Serializable::Strict
 
+    include Client::APIWrapper::Endpoint::Fetch(PlaceOS::Model::Trigger)
+    include Client::APIWrapper::Endpoint::Destroy
+    # include Client::APIWrapper::Endpoint::Create(PlaceOS::Model::Trigger)
+    
     getter base : String = "#{API_ROOT}/triggers"
 
     # CRUD Actions
@@ -14,36 +17,38 @@ module PlaceOS
       limit : Int = 20,
       offset : Int = 0
     )
-      get base, params: from_args, as: Array(Trigger)
+      get base, params: from_args, as: Array(PlaceOS::Model::Trigger)
     end
 
     def create(
       control_system_id : String,
       name : String,
-      description : String?,
-      actions : String?,
-      conditions : String?,
-      debounce_period : Int32?,
-      important : Bool?,
-      enable_webhook : Bool?,
-      supported_methods : Array(String)?
+      **args
+      # description : String?,
+      # actions : String?,
+      # conditions : String?,
+      # debounce_period : Int32?,
+      # important : Bool?,
+      # enable_webhook : Bool?,
+      # supported_methods : Array(String)?
     )
-      post base, body: from_args, as: Trigger
+      post base, body: from_args, as: PlaceOS::Model::Trigger
     end
 
     def update(
       id : String,
       control_system_id : String,
       name : String,
-      description : String?,
-      actions : String?,
-      conditions : String?,
-      debounce_period : Int32?,
-      important : Bool?,
-      enable_webhook : Bool?,
-      supported_methods : Array(String)?
+      **args
+      # description : String?,
+      # actions : String?,
+      # conditions : String?,
+      # debounce_period : Int32?,
+      # important : Bool?,
+      # enable_webhook : Bool?,
+      # supported_methods : Array(String)?
     )
-      put "#{base}/#{id}", body: from_args, as: Trigger
+      put "#{base}/#{id}", body: from_args, as: PlaceOS::Model::Trigger
     end
 
     # Unique Actions
