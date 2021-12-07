885ba4d04e42ef3f72b8d663b0f5f678ee089a84
tassja <xtassja@gmail.com>
2021-12-07 17:52:13 +1100

    feat(assets): init controller & spec

---
+++ spec/placeos/api_wrapper/assets_spec.cr
@@ -0,0 +1,98 @@
+require "../../spec_helper"
+
+module PlaceOS
+  describe Client::APIWrapper::Assets do
+    api = PlaceOS::Client::APIWrapper.new DOMAIN
+    client = Client::APIWrapper::Assets.new api
+
+    assets_json = <<-JSON
+    [
+      {
+        "name": "Place",
+        "description": null,
+        "purchase_date": "2016-02-15T10:20:30Z",
+        "identifier": "org",
+        "purchase_price": 1555995992,
+        "id": "asset-oOj2lGgsz",
+        "quantity": 0,
+        "in_use": 0
+      }
+    ]
+    JSON
+
+    assets = Array(JSON::Any).from_json(assets_json).map &.to_json
+
+    describe "search" do
+      it "enumerates all assets" do
+        WebMock
+          .stub(:get, DOMAIN + client.base)
+          .with(query: {"limit" => "20", "offset" => "0"}, headers: HEADERS)
+          .to_return(body: assets_json)
+        result = client.search
+        result.size.should eq(1)
+        asset = result.first
+        asset.should be_a(Client::API::Models::Asset)
+        asset.name.should eq("Place")
+      end
+
+      it "provides asset search" do
+        WebMock
+          .stub(:get, DOMAIN + client.base)
+          .with(query: {"q" => "Place", "limit" => "20", "offset" => "0"}, headers: HEADERS)
+          .to_return(body: assets_json)
+        result = client.search q: "Place"
+        result.size.should eq(1)
+        result.first.name.should eq("Place")
+      end
+    end
+
+    describe "#create" do
+      it "posts to the assets endpoint" do
+        body = {name: "Place", purchase_date: Time.utc(2016, 2, 15, 10, 20, 30)}
+        puts body.to_json
+        WebMock
+          .stub(:post, DOMAIN + client.base)
+          .with(
+            headers: HTTP::Headers{"Content-Type" => "application/json"},
+            body: body.to_json,
+          )
+          .to_return(body: assets.first)
+        result = client.create(**body)
+        result.should be_a(Client::API::Models::Asset)
+      end
+    end
+
+    pending "#retrieve" do
+      it "inspects a assets metadata" do
+        WebMock
+          .stub(:get, DOMAIN + "#{client.base}/asset-oOj2lGgsz")
+          .to_return(body: assets.first)
+        result = client.fetch "asset-oOj2lGgsz"
+        result.should be_a(Client::API::Models::Asset)
+      end
+    end
+
+    describe "#update" do
+      it "send a put request to the assets endpoint" do
+        WebMock
+          .stub(:put, DOMAIN + "#{client.base}/asset-oOj2lGgsz")
+          .with(
+            headers: HTTP::Headers{"Content-Type" => "application/json"},
+            body: {name: "Foo"}.to_json,
+          )
+          .to_return(body: assets.first)
+        result = client.update(id: "asset-oOj2lGgsz", name: "Foo")
+        result.should be_a(Client::API::Models::Asset)
+      end
+    end
+
+    describe "#delete" do
+      it "execs a delete request" do
+        WebMock
+          .stub(:delete, DOMAIN + "#{client.base}/asset-oOj2lGgsz")
+        result = client.destroy "asset-oOj2lGgsz"
+        result.should be_nil
+      end
+    end
+  end
+end
---
+++ src/placeos/api_wrapper/assets.cr
@@ -0,0 +1,65 @@
+require "../api/models/asset"
+require "./endpoint"
+
+module PlaceOS
+  class Client::APIWrapper::Assets < Client::APIWrapper::Endpoint
+    include Client::APIWrapper::Endpoint::Fetch(Asset)
+    include Client::APIWrapper::Endpoint::Destroy
+
+    getter base : String = "#{API_ROOT}/assets"
+
+    # Management
+    ###########################################################################
+
+    # Creates a new asset.
+    def create(
+      name : String,
+      purchase_date : Time,
+      identifier : String? = nil,
+      purchase_price : Int32? = nil
+    )
+      post base, body: from_args, as: API::Models::Asset
+    end
+
+    # Updates asset attributes or configuration.
+    def update(
+      id : String,
+      name : String? = nil,
+      purchase_date : Time? = nil,
+      identifier : String? = nil,
+      purchase_price : Int32? = nil
+    )
+      put "#{base}/#{id}", body: from_args, as: API::Models::Asset
+    end
+
+    # Search
+    ###########################################################################
+
+    # List or search for assets.
+    #
+    # Results maybe filtered by specifying a query - *q* - to search across asset
+    # attributes. A small query language is supported within this:
+    #
+    # Operator | Action
+    # -------- | ------
+    # `+`      | Matches both terms
+    # `|`      | Matches either terms
+    # `-`      | Negates a single token
+    # `"`      | Wraps tokens to form a phrase
+    # `(`  `)` | Provides precedence
+    # `~N`     | Specifies edit distance (fuzziness) after a word
+    # `~N`     | Specifies slop amount (deviation) after a phrase
+    #
+    # Up to *limit* assets will be returned, with a paging based on *offset*.
+    #
+
+    def search(
+      q : String? = nil,
+      limit : Int = 20,
+      offset : Int = 0,
+      parent : String? = nil
+    )
+      get base, params: from_args, as: Array(API::Models::Asset)
+    end
+  end
+end
